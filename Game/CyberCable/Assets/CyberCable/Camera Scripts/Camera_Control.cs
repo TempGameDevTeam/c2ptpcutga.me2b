f4995a09767163b8843bd415ef8d2bcd11280db4
Benjamin Muscat <benjamin.muscat.12@um.edu.mt>
2015-07-27 21:05:23 +0200

    Game_Code
    1st commit of initial game code, consisting of basic movement, jumping
    and camera follow.

---
+++ Game/CyberCable/Assets/CyberCable/Camera Scripts/Camera_Control.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_Control : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform character;
+
+    private Vector3 moveTemp;
+
+    [SerializeField]
+    float speed = 3;
+    [SerializeField]
+    float xDifference;
+    [SerializeField]
+    float yDifference;
+    [SerializeField]
+    float moveThreshold;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (character.transform.position.x > transform.position.x)
+        {
+            xDifference = character.transform.position.x - transform.position.x;
+        }
+        else
+        {
+            xDifference = transform.position.x - character.transform.position.x;
+        }
+
+
+
+        if (character.transform.position.y > transform.position.y)
+        {
+            yDifference = character.transform.position.y - transform.position.y;
+        }
+        else
+        {
+            yDifference = transform.position.y - character.transform.position.y;
+        }
+
+        if (yDifference <= 1.5f)
+        {
+            moveTemp = character.transform.position;
+            moveTemp.y = 1;
+        }
+
+        if (yDifference >= 2f)
+        {
+            moveTemp.y = character.transform.position.y;
+        }
+
+        if (xDifference >= moveThreshold || yDifference >= moveThreshold )
+        {
+           
+            transform.position = Vector3.MoveTowards(transform.position, moveTemp, speed * Time.deltaTime);
+        }
+    }
+
+    
+}
---
+++ Game/CyberCable/Assets/CyberCable/Player Scripts/Char_Movement.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.ComponentModel;
+
+public class Char_Movement : MonoBehaviour {
+	// testing variables
+	public float speedcount=0f;
+	
+	
+	//setting default movement speed
+	public float verticalSpeed = 0.1f;
+	
+	//setting hoe high thre charachter will jump
+	public float jumpForce = 20f;
+	private bool isGrounded = true;
+    private bool isJumping = false;
+
+ 
+	public bool isRight;
+	public bool isLeft;
+	
+	//this will detect if it hits  ceiling or ground 
+	//if hit ceiling  the jump will stop
+	void OnCollisionEnter2D(Collision2D other)
+	{
+        if (other.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+        }
+    
+
+		if (other.gameObject.tag == "ceiling") {
+			isGrounded=false;
+		}
+		
+	}
+	
+	// this will slow the  jump  so that it would menimise the impact on the ceiling if any
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "jumpAccLimit") {
+			isGrounded = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //check to see if char is grounded
+        //if the char is not moving veritcally, then he is not jumping or falling from a jump
+        if (GetComponent<Rigidbody2D>().velocity.y == 0)
+        {
+            isGrounded = true;
+            isJumping = false;
+        }
+
+//      ================ BASIC MOVEMENTS ==============
+        //moving right
+        if (Input.GetKey(KeyCode.D) || Input.GetKey("right"))
+        {
+                speedcount = verticalSpeed * 1.0001f;
+                transform.Translate(verticalSpeed * 1.1f, 0, 0);
+        }
+
+        //moving left
+        if (Input.GetKey(KeyCode.A) || Input.GetKey("left"))
+        {
+                speedcount = verticalSpeed * 1.0001f;
+                transform.Translate(-verticalSpeed * 1.1f, 0, 0); 
+        }
+		
+		//calling jump method
+        if ((Input.GetKey(KeyCode.Space) && isGrounded || Input.GetKey("w") && isGrounded))
+        { 
+            Jump();
+        }	
+		
+	}
+	
+	void Jump()
+	{   
+        GetComponent<Rigidbody2D>().AddForce(new Vector3(0, jumpForce, 0), ForceMode2D.Force);
+        isJumping = true;
+        isGrounded = false;
+	}
+}
